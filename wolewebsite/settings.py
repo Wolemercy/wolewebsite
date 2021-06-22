9d94d105603e1eae281c7bfc49836ddc56798671
Oluwole Ajewole <wolemercy@gmail.com>
2021-06-22 16:41:19 +0000

    updated settings.py

--- wolewebsite/settings.py
+++ wolewebsite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import json
+
+with open('/etc/wolewebsite-config.json') as config_file:
+	config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa2-atuwcil*bur=yq!b-b*s0v)z^=-^9fd+8hm%nvq-x1$nx9'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
